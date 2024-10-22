939433a56e8077c8ab2c9948bdc441ae9db0bfae
K Rajendra prasad <krajendraprasad2020@gmail.com>
2024-10-23 00:45:54 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    topic_name=models.CharField(max_length=20,primary_key=True)
+    def __str__(self):
+        return self.topic_name
+class Webpages(models.Model):
+    topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    name=models.CharField(max_length=20)
+    url=models.CharField(max_length=20)
+    email=models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+class Accessrecord(models.Model):
+    name=models.CharField(max_length=100)
+    author=models.CharField(max_length=100)
+    date=models.DateField()
+    location=models.CharField(max_length=100,default='Bangalore')
+    def __str__(self):
+        return self.name
+    
---
+++ app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from app.models import *
+# Create your views here.
+from django.http import HttpResponse
+def form(request):
+    return render(request,'form.html')
+def insert_topic(request):
+    if request.method=='POST':
+        tn=request.POST['tn']
+        to=Topic.objects.get_or_create(topic_name=tn)
+        return HttpResponse(f'{tn} is created')
+    
+    return render(request,'insert_topic.html')
+def insert_webpages(request):
+    To=Topic.objects.all()
+    d={'topics':To}
+    if request.method=='POST':
+        tn=request.POST['topic']
+        to=Topic.objects.get(topic_name=tn)
+        n=request.POST['n']
+        u=request.POST['u']
+        e=request.POST['e']
+        wo=Webpages.objects.get_or_create(topic_name=to,name=n,url=u,email=e)
+        return HttpResponse('data is inserted')
+    return render(request,'insert_webpages.html',d)
+def select_multiple(request):
+    TO=Topic.objects.all()
+    d={'TO':TO}
+    if request.method=='POST':
+        MTN=request.POST.getlist('tn')
+        EQST=Webpages.objects.none()
+        for i in MTN:
+            EQST=EQST|Webpages.objects.filter(topic_name=i)
+        d1={'EQST':EQST}
+        return render(request,'display_webpages.html',d1)
+            
+        
+    return render(request,'select_multiple.html',d)
+
